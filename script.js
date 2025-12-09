eac952c1c383a63d080a0312e44b0df974cc8568
faridhakanda <faridakanda1729@gmail.com>
2025-12-09 15:00:42 +0600

    gsap with splitText

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const currentYear = new Date();
 document.getElementById("current_year").innerHTML = currentYear.getFullYear();
 
+// main_name = Farid Akanda
 
+gsap.registerPlugin(SplitText);
+let split = SplitText.create(".main_name", {type: "chars, words, line"});
+gsap.from(split.chars, {
+    duration: 0.5,
+    xPercent: -100,
+    autoAlpha: 0,
+    stagger: 0.2
+});
 // gsap.to(".box", {
 //     rotation: 36,
 //     duration: 1,
