499ca11ffbe1d583127288ce23b2bc3cefe4449b
faridhakanda <faridakanda1729@gmail.com>
2025-12-09 14:40:22 +0600

    gsap removed for box

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const year = new Date();
-const y = year.getFullYear();
-document.getElementById("current_year").innerHTML = y;
+const currentYear = new Date();
+document.getElementById("current_year").innerHTML = currentYear.getFullYear();
 
 
-gsap.to(".box", {
-    rotation: 36,
-    duration: 1,
-    x: 100
-});
+// gsap.to(".box", {
+//     rotation: 36,
+//     duration: 1,
+//     x: 100
+// });
